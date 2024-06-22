ea4f0fcd3b9edd92f1d35dca8928db1fab4facf8
Vishnukant Shukla <92075105+vishnukantshukla@users.noreply.github.com>
2024-06-23 00:01:42 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./layout/Home";
+import Login from "./layout/Login";
+import Register from "./layout/Register";
+import Success from "./layout/Success";
+import Cancel from "./layout/Cancel";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
